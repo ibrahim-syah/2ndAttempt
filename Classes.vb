12fc8ac0b4a4603d88c6e8af826b86224eb508de
Ibrahim Syah <ibrahim.syah.q@gmail.com>
2021-05-04 22:20:42 +0700

    Add project files.

---
+++ Classes.vb
@@ -0,0 +1,444 @@
+﻿Imports System.IO
+Public Enum StateArmoredArmadillo
+    StandArmored
+    WalkDebug
+    IntroAnimation
+    Guard
+    ShootArmored
+    JumpStartArmored
+    JumpArmored
+    JumpEndArmored
+    Rolling
+    RollingRecoveryArmored
+    RollingRecoveryEndArmored
+End Enum
+
+Public Enum StateArmoredArmadilloProjectile
+    Create
+    Horizontal
+End Enum
+
+
+Public Enum FaceDir
+    Left
+    Right
+End Enum
+
+Public Class CImage
+    Public Width As Integer
+    Public Height As Integer
+    Public Elmt(,) As System.Drawing.Color
+    Public ColorMode As Integer 'not used
+
+    Sub OpenImage(ByVal FName As String)
+        'open file .bmp, read file
+        Dim s As String
+        Dim L As Long
+        Dim BR As BinaryReader
+        Dim h, w, pos As Integer
+        Dim r, g, b As Integer
+        Dim pad As Integer
+
+        BR = New BinaryReader(File.Open(FName, FileMode.Open))
+
+        Try
+            BlockRead(BR, 2, s)
+
+            If s <> "BM" Then
+                MsgBox("Not a BMP file")
+            Else 'BMP file
+                BlockReadInt(BR, 4, L) 'size
+                'MsgBox("Size = " + CStr(L))
+                BlankRead(BR, 4) 'reserved
+                BlockReadInt(BR, 4, pos) 'start of data
+                BlankRead(BR, 4) 'size of header
+                BlockReadInt(BR, 4, Width) 'width
+                'MsgBox("Width = " + CStr(I.Width))
+                BlockReadInt(BR, 4, Height) 'height
+                'MsgBox("Height = " + CStr(I.Height))
+                BlankRead(BR, 2) 'color panels
+                BlockReadInt(BR, 2, ColorMode) 'colormode
+                If ColorMode <> 24 Then
+                    MsgBox("Not a 24-bit color BMP")
+                Else
+
+                    BlankRead(BR, pos - 30)
+
+                    ReDim Elmt(Width - 1, Height - 1)
+                    pad = (4 - (Width * 3 Mod 4)) Mod 4
+
+                    For h = Height - 1 To 0 Step -1
+                        For w = 0 To Width - 1
+                            BlockReadInt(BR, 1, b)
+                            BlockReadInt(BR, 1, g)
+                            BlockReadInt(BR, 1, r)
+                            Elmt(w, h) = Color.FromArgb(r, g, b)
+
+                        Next
+                        BlankRead(BR, pad)
+
+                    Next
+
+                End If
+
+            End If
+
+        Catch ex As Exception
+            MsgBox("Error")
+
+        End Try
+
+        BR.Close()
+
+
+    End Sub
+
+
+    Sub CreateMask(ByRef Mask As CImage)
+        'create mask from *this*
+        Dim i, j As Integer
+
+        Mask = New CImage
+        Mask.Width = Width
+        Mask.Height = Height
+
+        ReDim Mask.Elmt(Mask.Width - 1, Mask.Height - 1)
+
+        For i = 0 To Width - 1
+            For j = 0 To Height - 1
+                If Elmt(i, j).R = 0 And Elmt(i, j).G = 0 And Elmt(i, j).B = 0 Then
+                    Mask.Elmt(i, j) = Color.FromArgb(255, 255, 255)
+                Else
+                    Mask.Elmt(i, j) = Color.FromArgb(0, 0, 0)
+                End If
+            Next
+        Next
+
+    End Sub
+
+
+    Sub CopyImg(ByRef Img As CImage)
+        'copies image to Img
+        Img = New CImage
+        Img.Width = Width
+        Img.Height = Height
+        ReDim Img.Elmt(Width - 1, Height - 1)
+
+        For i = 0 To Width - 1
+            For j = 0 To Height - 1
+                Img.Elmt(i, j) = Elmt(i, j)
+            Next
+        Next
+
+    End Sub
+
+End Class
+
+Public Class CCharacter
+    Public PosX, PosY As Double
+    Public Vx, Vy As Double
+    Public FrameIdx As Integer
+    Public CurrFrame As Integer
+    Public ArrSprites() As CArrFrame
+    Public IdxArrSprites As Integer
+    Public FDir As FaceDir
+    Public Destroy As Boolean = False
+
+    Public Const gravity = 1
+
+    'Public CurrState as ?
+
+    Public Sub GetNextFrame()
+        CurrFrame = CurrFrame + 1
+        If CurrFrame = ArrSprites(IdxArrSprites).Elmt(FrameIdx).MaxFrameTime Then
+            FrameIdx = FrameIdx + 1
+            If FrameIdx = ArrSprites(IdxArrSprites).N Then
+                FrameIdx = 0
+            End If
+            CurrFrame = 0
+
+        End If
+
+    End Sub
+
+    Public Overridable Sub Update()
+
+    End Sub
+
+
+End Class
+
+Public Class CCharArmoredArmadillo
+    Inherits CCharacter
+
+    Public isWalking As Boolean = False
+    Public isGuarding As Boolean = False
+    Public isShooting As Boolean = False
+    Public isInRollingAnimation As Boolean = False
+    Public isIntro As Boolean = True
+    Public wallbangCounter As Integer
+    Public CurrState As StateArmoredArmadillo
+
+    Public Sub State(state As StateArmoredArmadillo, idxspr As Integer)
+        CurrState = state
+        IdxArrSprites = idxspr
+        CurrFrame = 0
+        FrameIdx = 0
+
+    End Sub
+
+
+    Public Overrides Sub Update()
+        Select Case CurrState
+            Case StateArmoredArmadillo.StandArmored
+                GetNextFrame()
+                If isWalking Then
+                    State(StateArmoredArmadillo.WalkDebug, 1)
+                ElseIf isGuarding Then
+                    State(StateArmoredArmadillo.Guard, 3)
+                ElseIf isShooting Then
+                    State(StateArmoredArmadillo.ShootArmored, 4)
+                ElseIf isInRollingAnimation Then
+                    State(StateArmoredArmadillo.JumpStartArmored, 6)
+                End If
+            Case StateArmoredArmadillo.WalkDebug
+                PosX = PosX + Vx
+                If FDir = FaceDir.Left And isWalking = True Then
+                    Vx = -5
+                    PosX = PosX + Vx
+                    If PosX <= 45 Then
+                        Vx *= -1
+                        FDir = FaceDir.Right
+                    End If
+
+                ElseIf FDir = FaceDir.Right And isWalking = True Then
+                    Vx = 5
+                    PosX = PosX + Vx
+                    If PosX >= 300 Then
+                        Vx *= -1
+                        FDir = FaceDir.Left
+                    End If
+                End If
+                ' stopping walk
+                'stopping left
+                If FrameIdx = 0 And PosX >= PosX + Vx Then
+                    isWalking = False
+                    State(StateArmoredArmadillo.StandArmored, 0)
+                    'stopping right
+                ElseIf CurrFrame = 0 And PosX <= PosX + Vx Then
+                    isWalking = False
+                    State(StateArmoredArmadillo.StandArmored, 0)
+                End If
+                GetNextFrame()
+            Case StateArmoredArmadillo.IntroAnimation
+                GetNextFrame()
+                If CurrFrame = 4 And FrameIdx = 5 Then
+                    State(StateArmoredArmadillo.StandArmored, 0)
+                End If
+
+            Case StateArmoredArmadillo.ShootArmored
+                If FrameIdx = 0 And CurrFrame = 2 Then
+                    'TODO: shoot projectile
+
+                ElseIf FrameIdx = 1 And CurrFrame = 1 Then
+                    isShooting = False
+                    State(StateArmoredArmadillo.StandArmored, 0)
+                End If
+                GetNextFrame()
+
+            Case StateArmoredArmadillo.Guard
+                'TODO: create a hit event and deflect state
+                If isGuarding = False Then
+                    State(StateArmoredArmadillo.StandArmored, 0)
+                End If
+                GetNextFrame()
+
+            Case StateArmoredArmadillo.JumpStartArmored
+                Vy = -10
+                Vx = 0
+                GetNextFrame()
+                State(StateArmoredArmadillo.JumpArmored, 7)
+            Case StateArmoredArmadillo.JumpArmored
+                PosX = PosX + Vx
+                PosY = PosY + Vy
+                Vy = Vy + gravity
+                GetNextFrame()
+                If FrameIdx = 4 Then
+                    State(StateArmoredArmadillo.JumpEndArmored, 8)
+                End If
+
+            Case StateArmoredArmadillo.JumpEndArmored
+                PosX = PosX + Vx
+                PosY = PosY + Vy
+                Vy = Vy + gravity
+                GetNextFrame()
+                If PosY >= 238 And Vy > 0 Then
+                    State(StateArmoredArmadillo.Rolling, 9)
+                    PosY = 238
+                    Vx = 0
+                    Vy = 0
+                    wallbangCounter = 0
+                End If
+
+            Case StateArmoredArmadillo.Rolling
+                GetNextFrame()
+                If isIntro Then
+                    PosY = PosY + Vy
+                    Vy = Vy + gravity
+                    If PosY >= 235 Then
+                        Vy = -10
+                        State(StateArmoredArmadillo.RollingRecoveryArmored, 10)
+                    End If
+                ElseIf FDir = FaceDir.Left Then
+                    Vx = -10
+                    PosX = PosX + Vx
+                    If PosX <= 45 Then
+                        Vx *= -1
+                        FDir = FaceDir.Right
+                        wallbangCounter = wallbangCounter + 1
+                    End If
+
+                ElseIf FDir = FaceDir.Right Then
+                    Vx = 10
+                    PosX = PosX + Vx
+                    If PosX >= 300 Then
+                        Vx *= -1
+                        FDir = FaceDir.Left
+                        wallbangCounter = wallbangCounter + 1
+                    End If
+
+                    If wallbangCounter >= 3 Then
+                        GetNextFrame()
+                        If Vx > 0 Then
+                            Vx = 1
+                        Else
+                            Vx = -1
+                        End If
+                        Vy = -10
+                        State(StateArmoredArmadillo.RollingRecoveryArmored, 10)
+                    End If
+                End If
+                PosX = PosX + Vx
+
+            Case StateArmoredArmadillo.RollingRecoveryArmored
+                PosX = PosX + Vx
+                PosY = PosY + Vy
+                Vy = Vy + gravity
+                GetNextFrame()
+                If FrameIdx = 6 Then
+                    State(StateArmoredArmadillo.RollingRecoveryEndArmored, 11)
+                End If
+
+            Case StateArmoredArmadillo.RollingRecoveryEndArmored
+                PosX = PosX + Vx
+                PosY = PosY + Vy
+                Vy = Vy + gravity
+                GetNextFrame()
+                If PosY >= 238 And Vy > 0 Then
+                    State(StateArmoredArmadillo.StandArmored, 0)
+                    PosY = 238
+                    Vx = 0
+                    Vy = 0
+                    If isIntro Then
+                        isIntro = False
+                        State(StateArmoredArmadillo.IntroAnimation, 2)
+                    Else
+                        State(StateArmoredArmadillo.StandArmored, 0)
+                    End If
+                    isInRollingAnimation = False
+                End If
+
+        End Select
+
+    End Sub
+End Class
+
+Public Class CCharArmoredArmadilloProjectile
+    Inherits CCharacter
+
+    Public CurrState As StateArmoredArmadilloProjectile
+
+    Public Sub State(state As StateArmoredArmadilloProjectile, idxspr As Integer)
+        CurrState = state
+        IdxArrSprites = idxspr
+        CurrFrame = 0
+        FrameIdx = 0
+
+    End Sub
+
+    Public Overrides Sub Update()
+        Select Case CurrState
+            Case StateArmoredArmadilloProjectile.Create
+                GetNextFrame()
+                If FrameIdx = 0 And CurrFrame = 0 Then
+                    State(StateArmoredArmadilloProjectile.Horizontal, 1)
+                End If
+
+            Case StateArmoredArmadilloProjectile.Horizontal
+                GetNextFrame()
+                PosX = PosX + Vx
+                If PosX >= 320 Or PosX <= 20 Then
+                    Destroy = True
+                End If
+                If FDir = FaceDir.Left Then
+                    Vx = -6
+                Else
+                    Vx = 6
+                End If
+
+        End Select
+    End Sub
+
+End Class
+
+
+Public Class CElmtFrame
+  Public CtrPoint As TPoint
+  Public Top, Bottom, Left, Right As Integer
+
+  Public MaxFrameTime As Integer
+
+  Public Sub New(ctrx As Integer, ctry As Integer, l As Integer, t As Integer, r As Integer, b As Integer, mft As Integer)
+    CtrPoint.x = ctrx
+    CtrPoint.y = ctry
+    Top = t
+    Bottom = b
+    Left = l
+    Right = r
+    MaxFrameTime = mft
+
+  End Sub
+End Class
+
+Public Class CArrFrame
+  Public N As Integer
+  Public Elmt As CElmtFrame()
+
+  Public Sub New()
+    N = 0
+    ReDim Elmt(-1)
+  End Sub
+
+  Public Overloads Sub Insert(E As CElmtFrame)
+    ReDim Preserve Elmt(N)
+    Elmt(N) = E
+    N = N + 1
+  End Sub
+
+  Public Overloads Sub Insert(ctrx As Integer, ctry As Integer, l As Integer, t As Integer, r As Integer, b As Integer, mft As Integer)
+    Dim E As CElmtFrame
+    E = New CElmtFrame(ctrx, ctry, l, t, r, b, mft)
+    ReDim Preserve Elmt(N)
+    Elmt(N) = E
+    N = N + 1
+
+  End Sub
+
+End Class
+
+Public Structure TPoint
+  Dim x As Integer
+  Dim y As Integer
+
+End Structure
+
---
+++ Form1.vb
@@ -0,0 +1,384 @@
+﻿Imports System.IO
+Imports System.Threading
+Imports System.Threading.Tasks
+
+
+Public Class Form1
+    Dim bmp As Bitmap
+    Dim Bg, Img As CImage
+    Dim SpriteMap As CImage
+    Dim SpriteMask As CImage
+    Dim AA_StandArmored,
+        AA_WalkDebug, 'only for debuggin purposes, armored armadillo can't actually walk in the canonical lore of megaman extended universe
+        AA_IntroAnimation,
+        AA_Guard,
+        AA_ShootArmored,
+        AA_JumpStartArmored,
+        AA_JumpArmored,
+        AA_JumpEndArmored,
+        AA_Rolling,
+        AA_RollingRecoveryArmored,
+        AA_RollingRecoveryEndArmored As CArrFrame
+    Dim AA_ProjCreate1, AA_ProjHorizontal As CArrFrame
+    Dim ListChar As New List(Of CCharacter)
+    Dim SM As CCharArmoredArmadillo
+
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'open image for background, assign to bg
+
+        Randomize()
+
+        Bg = New CImage
+        Bg.OpenImage("C:\Dev\visual-studio-vb\2ndAttempt\jajal.bmp")
+
+        Bg.CopyImg(Img)
+
+        SpriteMap = New CImage
+        SpriteMap.OpenImage("C:\Dev\visual-studio-vb\2ndAttempt\chara.bmp")
+
+        SpriteMap.CreateMask(SpriteMask)
+
+        'initialize sprites for Armored Armadillo
+        AA_StandArmored = New CArrFrame
+        AA_StandArmored.Insert(364, 24, 345, 4, 387, 44, 1)
+
+        AA_WalkDebug = New CArrFrame
+        AA_WalkDebug.Insert(94, 82, 70, 56, 123, 101, 1)
+
+
+        AA_IntroAnimation = New CArrFrame
+        AA_IntroAnimation.Insert(364, 24, 345, 4, 387, 44, 2)
+        AA_IntroAnimation.Insert(421, 24, 401, 4, 451, 43, 2)
+        AA_IntroAnimation.Insert(364, 24, 345, 4, 387, 44, 2)
+        AA_IntroAnimation.Insert(421, 24, 401, 4, 451, 43, 2)
+        AA_IntroAnimation.Insert(28, 82, 9, 62, 57, 101, 5)
+        AA_IntroAnimation.Insert(94, 82, 70, 56, 123, 101, 5)
+
+        AA_Guard = New CArrFrame
+        AA_Guard.Insert(28, 82, 9, 62, 57, 101, 5)
+
+        AA_ShootArmored = New CArrFrame
+        AA_ShootArmored.Insert(210, 84, 190, 62, 240, 103, 3)
+        AA_ShootArmored.Insert(264, 84, 246, 63, 295, 102, 2)
+
+        AA_JumpStartArmored = New CArrFrame
+        AA_JumpStartArmored.Insert(364, 24, 345, 4, 387, 44, 1)
+
+        AA_JumpArmored = New CArrFrame
+        AA_JumpArmored.Insert(315, 22, 293, 3, 338, 55, 2) 'refer to video for how long(frame) the jump is
+        AA_JumpArmored.Insert(29, 137, 4, 114, 55, 158, 2)
+        AA_JumpArmored.Insert(315, 22, 293, 3, 338, 55, 2)
+        AA_JumpArmored.Insert(214, 21, 195, 3, 234, 54, 2)
+        AA_JumpArmored.Insert(167, 23, 153, 3, 188, 41, 2)
+
+        AA_JumpEndArmored = New CArrFrame
+        AA_JumpEndArmored.Insert(167, 23, 153, 3, 188, 41, 1)
+
+        AA_Rolling = New CArrFrame
+        AA_Rolling.Insert(23, 20, 8, 5, 39, 34, 1)
+        AA_Rolling.Insert(59, 17, 45, 2, 75, 32, 1)
+        AA_Rolling.Insert(96, 19, 81, 3, 112, 34, 1)
+        AA_Rolling.Insert(132, 15, 116, 3, 146, 28, 1)
+
+        AA_RollingRecoveryArmored = New CArrFrame
+        AA_RollingRecoveryArmored.Insert(23, 20, 8, 5, 39, 34, 2)
+        AA_RollingRecoveryArmored.Insert(59, 17, 45, 2, 75, 32, 2)
+        AA_RollingRecoveryArmored.Insert(96, 19, 81, 3, 112, 34, 2)
+        AA_RollingRecoveryArmored.Insert(132, 15, 116, 3, 146, 28, 2)
+        AA_RollingRecoveryArmored.Insert(167, 23, 153, 3, 188, 41, 2)
+        AA_RollingRecoveryArmored.Insert(214, 21, 195, 3, 234, 54, 2)
+        AA_RollingRecoveryArmored.Insert(315, 22, 293, 3, 338, 55, 2)
+
+        AA_RollingRecoveryEndArmored = New CArrFrame
+        AA_RollingRecoveryEndArmored.Insert(315, 22, 293, 3, 338, 55, 5)
+
+        SM = New CCharArmoredArmadillo
+        ReDim SM.ArrSprites(11)
+        SM.ArrSprites(0) = AA_StandArmored
+        SM.ArrSprites(1) = AA_WalkDebug
+        SM.ArrSprites(2) = AA_IntroAnimation
+        SM.ArrSprites(3) = AA_Guard
+        SM.ArrSprites(4) = AA_ShootArmored
+        '5 is missing
+        SM.ArrSprites(6) = AA_JumpStartArmored
+        SM.ArrSprites(7) = AA_JumpArmored
+        SM.ArrSprites(8) = AA_JumpEndArmored
+        SM.ArrSprites(9) = AA_Rolling
+        SM.ArrSprites(10) = AA_RollingRecoveryArmored
+        SM.ArrSprites(11) = AA_RollingRecoveryEndArmored
+
+        SM.PosX = 280
+        SM.PosY = 50
+        SM.Vx = 0
+        SM.Vy = 10
+        SM.State(StateArmoredArmadillo.Rolling, 9)
+        SM.FDir = FaceDir.Left
+
+        ListChar.Add(SM)
+
+        'initialize sprites for Sprite Projectiles
+        AA_ProjCreate1 = New CArrFrame
+        'AA_ProjCreate1.Insert(329, 81, 321, 73, 336, 88, 1)
+        'AA_ProjCreate1.Insert(346, 81, 339, 75, 352, 88, 1)
+        'AA_ProjCreate1.Insert(363, 81, 356, 73, 372, 90, 1)
+        AA_ProjCreate1.Insert(433, 81, 425, 71, 446, 91, 1)
+
+        AA_ProjHorizontal = New CArrFrame
+        AA_ProjHorizontal.Insert(384, 81, 377, 73, 395, 88, 1)
+        AA_ProjHorizontal.Insert(406, 81, 399, 73, 419, 88, 1)
+        AA_ProjHorizontal.Insert(363, 82, 356, 73, 372, 90, 1)
+
+        'TODO: add on hit sprite
+
+
+        bmp = New Bitmap(Img.Width, Img.Height)
+
+
+        DisplayImg()
+        ResizeImg()
+
+
+
+
+        Timer1.Enabled = True
+    End Sub
+
+    Sub PutSprites()
+        Dim cc As CCharacter
+
+        'set background
+        'Dim w, h As Integer
+        'w = Img.Width - 1
+        'h = Img.Height - 1
+
+        'Parallel.For(0, w - 1, _
+        '   Sub(i)
+        '     Parallel.For(0, h - 1, _
+        '       Sub(j)
+        '         For j = 0 To h - 1
+        '           Img.Elmt(i, j) = Bg.Elmt(i, j)
+        '         Next
+
+        '       End Sub)
+        '   End Sub)
+
+
+        For i = 0 To Img.Width - 1
+            For j = 0 To Img.Height - 1
+                Img.Elmt(i, j) = Bg.Elmt(i, j)
+            Next
+        Next
+
+
+        For Each cc In ListChar
+            Dim EF As CElmtFrame = cc.ArrSprites(cc.IdxArrSprites).Elmt(cc.FrameIdx)
+            Dim spritewidth = EF.Right - EF.Left
+            Dim spriteheight = EF.Bottom - EF.Top
+            If cc.FDir = FaceDir.Left Then
+                Dim spriteleft As Integer = cc.PosX - EF.CtrPoint.x + EF.Left
+                Dim spritetop As Integer = cc.PosY - EF.CtrPoint.y + EF.Top
+                'set mask
+                For i = 0 To spritewidth
+                    For j = 0 To spriteheight
+                        Img.Elmt(spriteleft + i, spritetop + j) = OpAnd(Img.Elmt(spriteleft + i, spritetop + j), SpriteMask.Elmt(EF.Left + i, EF.Top + j))
+                    Next
+                Next
+
+                'set sprite
+                For i = 0 To spritewidth
+                    For j = 0 To spriteheight
+                        Img.Elmt(spriteleft + i, spritetop + j) = OpOr(Img.Elmt(spriteleft + i, spritetop + j), SpriteMap.Elmt(EF.Left + i, EF.Top + j))
+                    Next
+                Next
+            Else 'facing right
+                Dim spriteleft = cc.PosX + EF.CtrPoint.x - EF.Right
+                Dim spritetop = cc.PosY - EF.CtrPoint.y + EF.Top
+                'set mask
+                For i = 0 To spritewidth
+                    For j = 0 To spriteheight
+                        Img.Elmt(spriteleft + i, spritetop + j) = OpAnd(Img.Elmt(spriteleft + i, spritetop + j), SpriteMask.Elmt(EF.Right - i, EF.Top + j))
+                    Next
+                Next
+
+                'set sprite
+                For i = 0 To spritewidth
+                    For j = 0 To spriteheight
+                        Img.Elmt(spriteleft + i, spritetop + j) = OpOr(Img.Elmt(spriteleft + i, spritetop + j), SpriteMap.Elmt(EF.Right - i, EF.Top + j))
+                    Next
+                Next
+
+            End If
+
+        Next
+
+
+    End Sub
+
+    Sub DisplayImg()
+        'display bg and sprite on picturebox
+        Dim i, j As Integer
+        'Dim sw As New System.Diagnostics.Stopwatch
+
+        'sw.Start()
+
+        PutSprites()
+
+
+
+        Dim rect As New Rectangle(0, 0, bmp.Width, bmp.Height)
+        Dim bmpdata As System.Drawing.Imaging.BitmapData = bmp.LockBits(rect,
+     System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat)
+
+        'Get the address of the first line.
+        Dim ptr As IntPtr = bmpdata.Scan0
+
+
+        'Declare an array to hold the bytes of the bitmap.
+        Dim bytes As Integer = Math.Abs(bmpdata.Stride) * bmp.Height
+        Dim rgbvalues(bytes) As Byte
+
+
+        'Copy the RGB values into the array.
+        System.Runtime.InteropServices.Marshal.Copy(ptr, rgbvalues, 0, bytes)
+
+        Dim n As Integer = 0
+        Dim col As System.Drawing.Color
+
+        'Set every third value to 255. A 24bpp bitmap will look red.  
+        For j = 0 To Img.Height - 1
+            For i = 0 To Img.Width - 1
+                col = Img.Elmt(i, j)
+                rgbvalues(n) = col.B
+                rgbvalues(n + 1) = col.G
+                rgbvalues(n + 2) = col.R
+                rgbvalues(n + 3) = col.A
+
+                n = n + 4
+            Next
+        Next
+
+        'Copy the RGB values back to the bitmap
+        System.Runtime.InteropServices.Marshal.Copy(rgbvalues, 0, ptr, bytes)
+
+
+        'Unlock the bits.
+        bmp.UnlockBits(bmpdata)
+
+        'Timer1.Enabled = False
+
+        'MsgBox(CStr(bmp.GetPixel(0, 0).A) + " " + CStr(bmp.GetPixel(0, 0).R) + " " + CStr(bmp.GetPixel(0, 0).G) + " " + CStr(bmp.GetPixel(0, 0).B))
+        'MsgBox(CStr(bmp.GetPixel(1, 0).A) + " " + CStr(bmp.GetPixel(1, 0).R) + " " + CStr(bmp.GetPixel(1, 0).G) + " " + CStr(bmp.GetPixel(1, 0).B))
+
+
+
+        PictureBox1.Refresh()
+
+        PictureBox1.Image = bmp
+        PictureBox1.Width = bmp.Width
+        PictureBox1.Height = bmp.Height
+        PictureBox1.Top = 0
+        PictureBox1.Left = 0
+
+
+
+
+
+    End Sub
+
+
+
+    Sub ResizeImg()
+        Dim w, h As Integer
+
+        w = PictureBox1.Width
+        h = PictureBox1.Height
+
+        Me.ClientSize = New Size(w, h)
+
+    End Sub
+
+
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim CC As CCharacter
+
+
+        PictureBox1.Refresh()
+
+        For Each CC In ListChar
+            CC.Update()
+
+        Next
+
+        If SM.CurrState = StateArmoredArmadillo.ShootArmored And SM.CurrFrame = 2 Then
+            CreateArmoredArmadilloProjectile(1)
+        End If
+
+        Dim Listchar1 As New List(Of CCharacter)
+
+        For Each CC In ListChar
+            If Not CC.Destroy Then
+                Listchar1.Add(CC)
+            End If
+        Next
+
+        ListChar = Listchar1
+
+        DisplayImg()
+
+    End Sub
+
+    Sub CreateArmoredArmadilloProjectile(n As Integer)
+        Dim SP As CCharArmoredArmadilloProjectile
+
+        SP = New CCharArmoredArmadilloProjectile
+        If SM.FDir = FaceDir.Left Then
+            SP.PosX = SM.PosX - 20
+            SP.FDir = FaceDir.Left
+        Else
+            SP.PosX = SM.PosX + 20
+            SP.FDir = FaceDir.Right
+        End If
+
+        SP.PosY = SM.PosY - 10
+
+        SP.Vx = 0
+        SP.Vy = 0
+        SP.CurrState = StateArmoredArmadilloProjectile.Create
+        ReDim SP.ArrSprites(2)
+        If n = 1 Then
+            SP.ArrSprites(0) = AA_ProjCreate1
+        End If
+        SP.ArrSprites(1) = AA_ProjHorizontal
+
+        ListChar.Add(SP)
+    End Sub
+
+    Private Sub ArmoredArmadillo_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Left And SM.CurrState = StateArmoredArmadillo.StandArmored Then
+            SM.isWalking = True
+            SM.FDir = FaceDir.Left
+        ElseIf e.KeyCode = Keys.Right And SM.CurrState = StateArmoredArmadillo.StandArmored Then
+            SM.isWalking = True
+            SM.FDir = FaceDir.Right
+        ElseIf e.KeyCode = Keys.Down And SM.CurrState = StateArmoredArmadillo.StandArmored Then
+            SM.isGuarding = True
+        ElseIf e.KeyCode = Keys.S And SM.CurrState = StateArmoredArmadillo.StandArmored Then
+            SM.isShooting = True
+        ElseIf e.KeyCode = Keys.Space And SM.CurrState = StateArmoredArmadillo.StandArmored Then
+            SM.isInRollingAnimation = True
+        End If
+    End Sub
+
+    Private Sub ArmoredArmadillo_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+
+        If e.KeyCode = Keys.Down Then
+            SM.isGuarding = False
+        End If
+    End Sub
+
+End Class
